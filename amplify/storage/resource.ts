ea55f9d791502de77c10077c55f3cd735f5e1c49
Arvind <arvind.menon@ext.deltacapita.com>
2025-05-27 20:27:51 +0530

    testing bucket

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,19 +1,12 @@
 import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
-  name: "storage-browser-test",
+  name: "delta-qwyn-package-dc",
   access: (allow: any) => ({
-    'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
-    'media-readonly/*': [allow.authenticated.to(['read'])],
-    'shared-folder-readwrite/*': [
-      allow.authenticated.to(['read', 'write'])
-    ],
-    'protected-useronlyreadwritedelete/{entity_id}/*': [
-      allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'private-useronlyreadwritedelete/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
+    'client-a/*': [allow.authenticated.to(['read', 'write'])],
+    // 'media-readonly/*': [allow.authenticated.to(['read'])],
+    // 'shared-folder-readwrite/*': [
+    //   allow.authenticated.to(['read', 'write'])
+    // ],
   })
 });
