256032d59486f49fdc0663d0791f9374a3a314b5
Arvind <arvind.menon@ext.deltacapita.com>
2025-05-28 12:09:16 +0530

    removed multiple ref to storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -6,8 +6,7 @@ import { storage } from "./storage/resource";
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
-  auth,
-  storage,
+  auth
 });
 
 backend.addOutput({
